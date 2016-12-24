9e46caa0e1101a86243949d333317fb42c5ea6aa
Saravanan C <yahoosaran@gmail.com>
2016-12-24 07:52:54 +0530

    updated test js
    Signed-off-by: Saravanan C <yahoosaran@gmail.com>

--- js/test_js.js
+++ js/test_js.js
@@ -2,3 +2,4 @@ test javascript file
 added in "test local branch at 7:27 am"
 added in "test local branch 1 at 7:35 am"
 added in "test local branch 1 at 7:41 am"
+added in "test local branch 1 at 7:52 am"
